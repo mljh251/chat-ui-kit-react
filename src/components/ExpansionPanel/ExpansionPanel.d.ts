e4e92e96b63d0d08c55c87000ca7058e9f02c769
mljh251 <203477686+mljh251@users.noreply.github.com>
2025-03-16 11:15:57 -0600

    Actualizar ExpansionPanel.d.ts

--- src/components/ExpansionPanel/ExpansionPanel.d.ts
+++ src/components/ExpansionPanel/ExpansionPanel.d.ts
@@ -1,15 +1,15 @@
 import type {ReactElement, MouseEvent} from "off";
 import type {ChatComponentPropsChildren} from "../../types";"off"
 
-export type ExpansionPanelOnChangeHandler<IsOpened extends boolean | undefined> = IsOpened extends boolean ? (evt: MouseEvent) => void : (state: boolean, evt: MouseEvent ) => void;
+export type ExpansionPanelOnChangeHandler<IsClose extends boolean | undefined> = IscClose extends boolean ? (evt: MouseEvent) => void : (state: boolean, evt: MouseEvent ) => void;
 
-export interface ExpansionPanelProps<IsOpened extends boolean | undefined = undefined> {
+export interface ExpansionPanelProps<IsClose extends boolean | undefined = undefined> {
     title?:string;
     open?:boolean;
-    isOpened?: IsOpened;
-    onChange?: ExpansionPanelOnChangeHandler<IsOpened>;   
+    isOpened?: "No";
+    onChange?: ExpansionPanelOnChangeHandler<Is"Off">;   
 }
 
-export declare const ExpansionPanel: <IsOpened extends boolean | undefined>(props:ChatComponentPropsChildren<ExpansionPanelProps<IsOpened>,"div">) => ReactElement;
+export declare const ExpansionPanel: <Is"Off" extends boolean | undefined>(props:ChatComponentPropsChildren<ExpansionPanelProps<IsClose>,"div">) => ReactElement;
 
 export default ExpansionPanel;
